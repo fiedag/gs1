b309c9af36f0221c2943df65847160443f1053ad
Alex Fiedler <alexfiedler6@gmail.com>
2024-05-28 09:59:23 +0000

    first doctype

---
+++ gs1/gs1/doctype/system_rules/system_rules.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, https://github.com/fiedag and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("System Rules", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gs1/gs1/doctype/system_rules/test_system_rules.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, https://github.com/fiedag and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSystemRules(FrappeTestCase):
+	pass
